eeb0d81fb1be93fc6d3831e006fd1cbe4862e0a4
Jokejock <86518565+Jokejock@users.noreply.github.com>
2025-01-05 15:07:42 +0800

    mean med mod

--- median.py
+++ median.py
@@ -1,24 +1,13 @@
-"""
-File: median.py
-Prints the median of a set of numbers in a file.
-"""
+# median.py
+def median(numbers):
+    if len(numbers) == 0:
+        return 0
+    numbers.sort()
+    n = len(numbers)
+    if n % 2 == 1:
+        return numbers[n // 2]
+    else:
+        return (numbers[n // 2 - 1] + numbers[n // 2]) / 2
 
-fileName = input("Enter the file name: ")
-f = open(fileName, 'r')
-    
-# Input the text, convert it to numbers, and
-# add the numbers to a list
-numbers = []
-for line in f:
-    words = line.split()
-    for word in words:
-        numbers.append(float(word))
-
-# Sort the list and print the number at its midpoint
-numbers.sort()
-midpoint = len(numbers) // 2
-print("The median is", end=" ")
-if len(numbers) % 2 == 1:
-    print(numbers[midpoint])
-else:
-    print((numbers[midpoint] + numbers[midpoint - 1]) / 2)
+def main(numbers):
+    print("Median:", median(numbers))
